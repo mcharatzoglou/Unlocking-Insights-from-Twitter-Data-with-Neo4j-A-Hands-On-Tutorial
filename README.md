11c7de6d8e4f87edee610cacc475d893fa19f4f1
Michalis Charatzoglou <micharatz97@gmail.com>
2023-03-24 11:28:02 +0200

    get distinct users with followers

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Web-Mining DWS201
+
+### setup:
+<i>Python 3.9 required</i><br>
+<code>pip install -r requirements.txt</code>
--- python_mongo_utils/mongo_python.py
+++ python_mongo_utils/mongo_python.py
@@ -1,5 +1,6 @@
 import pymongo
 
+
 def set_mongo_connection(connection_string,database,collection):
     client = pymongo.MongoClient(connection_string)
     db = client[database]
@@ -9,25 +10,34 @@ def set_mongo_connection(connection_string,database,collection):
 
 def get_users(collection):
     documents = collection.find({"includes.users": {"$exists": True}})
-    get_user_list = []
-    for item in documents:
-        for user in item['includes']['users']:
-            get_user_list.append({
+    user_list = []
+    for obj in documents:
+        for user in obj['includes']['users']:
+            user_list.append({
                 'id': user['id'],
-                'followers': user['public_metrics']['followers_count']
+                'followers': user['public_metrics']['followers_count'],
+                'username': user['username'],
+                # this is the date that the Twitter API logged the tweet
+                'info_last_updated': obj['data']['created_at_converted']
             })
-    return get_user_list
+    # sort user_list by info_last_updated in descending order
+    user_list.sort(key=lambda x: x['info_last_updated'], reverse=True)
 
+    unique_users = {}
+    for user in user_list:
+        if user['id'] not in unique_users:
+            unique_users[user['id']] = user
 
-client,database,collection = set_mongo_connection(
-    "mongodb://localhost:27017/",
-    "local",
-    "test"
-)
+    # convert unique_users dictionary to a list of user objects
+    distinct_list_by_id = list(unique_users.values())
 
-users = get_users(collection)
-for user in users:
-    print(user)
+    # logs
+    # for item in distinct_list_by_id:
+    #     print(item)
+    # print(str(len(user_list)) + " users found")
+    # print(str(len(distinct_list_by_id)) + " distinct users kept")
+
+    return distinct_list_by_id
 
 
 
